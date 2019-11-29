d5ede45a5124ed3ce853f22d712a0d090761575b
Alberto Ivo <albertoivo@gmail.com>
2019-11-29 14:10:26 -0300

    python subindo com flask run

--- docker/dockerfile-flask/entrypoint.sh
+++ docker/dockerfile-flask/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 export FLASK_ENV=development
-export FLASK_APP=models
+export FLASK_APP=app
 
-cd /app || exit
+#flask db migrate
+#flask db upgrade
 
-flask db migrate
-flask db upgrade
+#gunicorn -b :5000 app:app
 
-gunicorn -b :5000 app:app
-
-
+flask run --host=0.0.0.0
