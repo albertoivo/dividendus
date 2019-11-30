d14f0cab989c01e2518c3237750ea0a26525d4d2
Alberto Ivo Vieira <albertoivo@gmail.com>
2019-11-30 13:25:31 -0300

    env variables and flask-migrate

--- docker/dockerfile-flask/entrypoint.sh
+++ docker/dockerfile-flask/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 export FLASK_ENV=development
-export FLASK_APP=app
 
+export FLASK_APP=models
+
+flask db init
 flask db migrate
 flask db upgrade
 
 #gunicorn -b :5000 app:app
 
+export FLASK_APP=app
 flask run --host=0.0.0.0
