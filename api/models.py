db8bf1cb37fb90666bdfc32066b2e4d811023d07
Alberto Ivo <albertoivo@gmail.com>
2019-12-03 13:19:04 -0300

    flask-migrate e ordem do postgres

--- api/models.py
+++ api/models.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects import postgresql
 
 from config import Config
 
 # -------------------------------------------------------------------------- #
 # Config & Flask-Migrate.
 # -------------------------------------------------------------------------- #
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,15 +17,26 @@ migrate = Migrate(app, db)
 
 
 # -------------------------------------------------------------------------- #
+# Enum.
+# -------------------------------------------------------------------------- #
+
+
+operations = ('Buy', 'Sell')
+operations_enum = postgresql.ENUM(*operations, name='operation')
+
+
+# -------------------------------------------------------------------------- #
 # Models.
 # -------------------------------------------------------------------------- #
+
 
 class User(db.Model):
     """User Entity"""
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String)
+    email = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    stocks = db.relationship('Stock', lazy=True, cascade='delete')
 
     def create(self):
         db.session.add(self)
@@ -36,7 +49,6 @@ class User(db.Model):
     @property
     def serialize(self):
         return {
-            'id': self.id,
             'name': self.name,
             'email': self.email
         }
@@ -45,12 +57,12 @@ class User(db.Model):
 class Stock(db.Model):
     """Stock entity"""
 
-    ticker = db.Column(db.String, primary_key=True)
-    country = db.Column(db.String)
-    broker = db.Column(db.String)
-    date = db.Column(db.Date)
-    price = db.Column(db.String)
-    tax = db.Column(db.Float)
+    ticker = db.Column(db.String(10), primary_key=True)
+    description = db.Column(db.String(250))
+
+    user_email = db.Column(db.String, db.ForeignKey('user.email'))
+
+    stock_details = db.relationship('StockDetail', lazy=True, cascade='delete')
 
     def create(self):
         db.session.add(self)
@@ -64,8 +76,28 @@ class Stock(db.Model):
     def serialize(self):
         return {
             'ticker': self.ticker,
-            'broker': self.broker,
+            'description': self.description,
+            'user_email': self.user_email
+        }
+
+
+class StockDetail(db.Model):
+    """Stock Detail entity."""
+
+    stock_ticker = db.Column(db.String, db.ForeignKey('stock.ticker'), primary_key=True)
+    date = db.Column(db.Date, primary_key=True)
+    operation = db.Column(operations_enum)
+    price = db.Column(db.Float)
+    tax = db.Column(db.Float)
+    country = db.Column(db.String)
+
+    @property
+    def serialize(self):
+        return {
+            'stock_id': self.stock_id,
             'date': self.date,
+            'operation': self.operation,
             'price': self.price,
-            'tax': self.tax
+            'tax': self.tax,
+            'country': self.country
         }
--- docker/dockerfile-flask/entrypoint.sh
+++ docker/dockerfile-flask/entrypoint.sh
@@ -4,18 +4,23 @@
 # while ! nc -z postgres 5432; do sleep 1; done;
 sleep 5
 
-# Flask-Migrate
 export FLASK_ENV=development
+
+# Flask-Migrate
+
 export FLASK_APP=models
 
-
 [ ! -d "migrations" ] && echo "Directory migrations DOES NOT exists." &&  flask db init
+
 flask db migrate
+
 [ -d "migrations" ] && echo "Directory migrations exists." && flask db upgrade
 
 
-#gunicorn -b :5000 app:app
+# gunicorn -b :5000 app:app
 
 # App - API
+
 export FLASK_APP=app
+
 flask run --host=0.0.0.0
